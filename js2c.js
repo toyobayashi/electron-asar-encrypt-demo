9221620b9919587d1aca7a15f0bdd3a9c560f4a0
toyobayashi <lifenglin314@outlook.com>
2020-03-02 18:22:33 +0800

    js2c

--- js2c.js
+++ js2c.js
@@ -1,6 +1,11 @@
 const fs = require('fs')
 const path = require('path')
-const terser = require('terser')
+let terser
+try {
+  terser = require('terser')
+} catch (_) {
+  process.exit(0)
+}
 
 var options = {}
 
